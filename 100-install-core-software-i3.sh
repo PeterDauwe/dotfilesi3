39444b5ed8031ffcf1b610be790d6b9c367479fd
PeterDauwe <bobo5290461@gmail.com>
2021-03-15 07:19:27 +0100

    Added electron for freetube

--- 100-install-core-software-i3.sh
+++ 100-install-core-software-i3.sh
@@ -30,6 +30,7 @@ sudo pacman -S --noconfirm --needed linux-headers
 sudo pacman -S --noconfirm --needed discord
 sudo pacman -S --noconfirm --needed smplayer
 sudo pacman -S --noconfirm --needed alacritty
+sudo pacman -S --noconfirm --needed electron
 
 if yay -Qi freetube &> /dev/null ; then 
       echo "Freetube is already installed"
