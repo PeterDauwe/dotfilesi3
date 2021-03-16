27940c3cbd64b6f1cd1619bc6b3261f45f324d03
PeterDauwe <bobo5290461@gmail.com>
2021-03-16 09:54:03 +0100

    up

--- 100-install-core-software-i3.sh
+++ 100-install-core-software-i3.sh
@@ -31,6 +31,7 @@ sudo pacman -S --noconfirm --needed discord
 sudo pacman -S --noconfirm --needed smplayer
 sudo pacman -S --noconfirm --needed alacritty
 sudo pacman -S --noconfirm --needed electron
+sudo pacman -S --noconfirm --needed simplescreenrecorder
 
 if yay -Qi freetube &> /dev/null ; then 
       echo "Freetube is already installed"
